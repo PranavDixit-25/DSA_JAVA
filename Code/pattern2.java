791426274c36c4d8ffa728967b13bc61b7e3767a
Pranav dixit <pranavking90@gmail.com>
2026-02-03 23:54:01 +0530

    Add files via upload
    variable side rectangle

---
+++ Code/pattern2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pattern2 {
+   public static void main(String[] args) {
+    System.out.println("enter the length of sides of rectangle");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    for(int i=1;i<=a;i++) {
+        for(int j=1;j<=b;j++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+   } 
+}
