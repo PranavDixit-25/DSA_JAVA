00cb13a57641dc526b3440be95e49c9287ad6b80
Pranav Dixit <pranav1467d@gmail.com>
2026-02-18 21:52:26 +0530

    added bridge.java

---
+++ Code/bridge.java
@@ -0,0 +1,21 @@
+public class bridge {
+    public static void main(String[] args) {
+        int n=5;
+        for(int k=1;k<=2*n-1;k++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        for(int i=1;i<=n-1;i++) {
+            for(int j=1;j<=n-i;j++) {
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*i-1;j++) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n-i;j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
