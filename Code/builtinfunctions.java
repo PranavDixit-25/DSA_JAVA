abc739e139f1924c7b5c6a834045dd5927beb055
Pranav Dixit <pranav1467d@gmail.com>
2026-02-19 23:05:27 +0530

    added builtinfunctions.java

---
+++ Code/builtinfunctions.java
@@ -0,0 +1,12 @@
+public class builtinfunctions {
+    public static void main(String[] args) {
+        // math. wale
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.cbrt(8));
+        System.out.println(Math.abs(-58));
+        System.out.println(Math.floor(5.666));
+        System.out.println(Math.ceil(5.666));
+        System.out.println(Math.min(3,4));
+        System.out.println(Math.max(3,4));
+    }
+}
