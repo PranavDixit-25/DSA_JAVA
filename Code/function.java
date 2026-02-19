dab229272068d65510473a3ffaf66a94b3d13840
Pranav Dixit <pranav1467d@gmail.com>
2026-02-19 23:43:38 +0530

    added function.java

---
+++ Code/function.java
@@ -0,0 +1,9 @@
+public class function {
+    public static void pranav() {
+        System.out.println("i m pranav");
+    }
+    public static void main(String[] args) {
+        pranav();
+    }
+    
+}
