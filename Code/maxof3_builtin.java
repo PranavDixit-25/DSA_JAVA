34b896c4ee07d47895798cea0c4e8c56e8e3c2fc
Pranav Dixit <pranav1467d@gmail.com>
2026-02-19 23:06:05 +0530

    added maxof3_builtin.java

---
+++ Code/maxof3_builtin.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class maxof3_builtin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 3 integers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d = Math.max(a,b);
+        int e = Math.max(d,c);
+        System.out.println("largest integer is:" + e);
+    }
+}
