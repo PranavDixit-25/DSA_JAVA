6976a1d8a469ce5c7c66dadc6d8f315e9aae2e96
Pranav Dixit <pranav1467d@gmail.com>
2026-02-26 01:40:10 +0530

    array1.java

--- Code/array1.java
+++ Code/array1.java
@@ -1,12 +1,20 @@
-class Main {
+import java.util.*;
+public class array1 {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        // int[] arr = {10, 20, 30, 40, 50};
 
-        int[] arr = {10, 20, 30, 40, 50};
+        //System.out.println("Array elements are:");
 
-        System.out.println("Array elements are:");
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        //for (int i = 0; i < arr.length; i++) {
+          //  System.out.println(arr[i]);
+    
+          int[] arr = new int[6];
+          for(int i=0;i<arr.length;i++) {
+            arr[i] = sc.nextInt();
+          }
+          for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+          }
         }
     }
-}
