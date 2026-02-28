dcd065795e2dcda4da99f457cb22ce277b52a6b1
Pranav Dixit <pranav1467d@gmail.com>
2026-02-28 23:11:46 +0530

    arr_reverse.java

--- Code/arr_reverse.java
+++ Code/arr_reverse.java
@@ -1,6 +1,15 @@
+import java.util.*;
 public class arr_reverse {
     public static void main(String[] args) {
-        int[] arr = {10 , 20 ,30 ,40 ,50};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("enter elements for your array");
+        for(int i=0;i<=n-1;i++) {
+            arr[i] = sc.nextInt();
+        }
         for (int i=arr.length-1;i>=0;i--) {
             System.out.print(arr[i] + " ");
         }
