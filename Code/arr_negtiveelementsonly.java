0c7f227e6fbab283ecc8aae9cd522164b0f6b714
Pranav Dixit <pranav1467d@gmail.com>
2026-03-01 23:27:02 +0530

    arr_negtiveelementsonly.java

---
+++ Code/arr_negtiveelementsonly.java
@@ -0,0 +1,9 @@
+public class arr_negtiveelementsonly {
+    public static void main(String[] args) {
+        int[] arr = {1,2,-3,4,-5};
+        for (int i=0;i<=arr.length-1;i++) {
+            if(arr[i]<0) System.out.print(arr[i]+" ");
+        }
+
+    }
+}
