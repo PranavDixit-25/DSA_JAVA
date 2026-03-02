044f9ac27b10272ec3761a6f0d95d5898cf0c3db
Pranav Dixit <pranav1467d@gmail.com>
2026-03-02 23:06:10 +0530

    arr_negtiveelementsonly.java

--- Code/arr_negtiveelementsonly.java
+++ Code/arr_negtiveelementsonly.java
@@ -1,6 +1,16 @@
+import java.util.*;
 public class arr_negtiveelementsonly {
     public static void main(String[] args) {
-        int[] arr = {1,2,-3,4,-5};
+         Scanner sc = new Scanner(System.in);
+         System.out.println("enter the size of the array");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("enter elements for your array");
+        for(int i=0;i<=n-1;i++) {
+            arr[i] = sc.nextInt();
+        }
+       
         for (int i=0;i<=arr.length-1;i++) {
             if(arr[i]<0) System.out.print(arr[i]+" ");
         }
