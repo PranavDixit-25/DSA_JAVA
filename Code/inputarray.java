ecccd693693c3bc0df326fae9fcbc326f23db46e
qamis2004 <qa.mis2004@gmail.com>
2026-09-13 19:55:25 +0530

    Updated DSA programs

--- Code/inputarray.java
+++ Code/inputarray.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 public class inputarray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,7 +14,21 @@ public class inputarray {
         for(int val : arr) {
             System.out.print(val +" ");
         }
-
-        
+        int sum=0;
+        for (int a=0;a<n;a++) {
+            sum = sum + arr[a];
+        }
+        System.out.println();
+        System.out.println("sum of array is" + sum);
+        int mul=1;
+        for (int b=0;b<n;b++) {
+            mul = mul * arr[b];
+        }
+        System.out.println();
+        System.out.println("mul of array is" + mul);
+        int max =0;
+        for (int c=0;c<n;c++) {
+            if (arr[c] >= max) {max = arr[c];}}
+            System.out.println("MAX element is" + max);
     }
 }
